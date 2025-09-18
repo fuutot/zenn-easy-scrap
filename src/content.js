af5c57f92c21800339f7111fb928bc77ef1a2956
fuutot <fuutt659@gmail.com>
2025-09-18 17:03:30 +0900

    add: ボタンを作成

--- src/content.js
+++ src/content.js
@@ -1,1 +1,11 @@
-console.log("Content script loaded");
+// ボタン要素を作成
+const button = document.createElement('button');
+button.textContent = '新規Scrap作成';
+
+// ボタンをページに追加
+document.body.appendChild(button);
+
+// ボタンのクリックイベント
+button.addEventListener('click', () => {
+  alert('ボタンがクリックされました');
+});
