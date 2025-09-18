7b84f0347a209eba479971ac77103dc1b4574213
fuutot <fuutt659@gmail.com>
2025-09-18 17:38:18 +0900

    update: Zennのスタイルを反映

--- src/content.js
+++ src/content.js
@@ -2,12 +2,22 @@
 const targetSelector = '.ThreadEditor_buttons__Y_Bk5';
 const targets = document.querySelectorAll(targetSelector);
 
+// 追加するボタンの設定
+const buttonClasses = [
+    "Button_primary__VcoA9",
+    "Button_baseStyle__Vhn6Y",
+    "Button_small__ErWhv",
+    "Button_fontBold__BN6Co"
+];
+const buttonText = '新規Scrap作成';
+
 // ターゲット要素が存在する場合、ボタンを追加
 if (targets.length > 0) {
   targets.forEach(target => {
     // 要素ごとにボタンを作成して追加
     const button = document.createElement('button');
-    button.textContent = '新規Scrap作成';
+    button.textContent = buttonText;
+    button.classList.add(...buttonClasses);
     button.addEventListener('click', () => {
       alert('ボタンがクリックされました');
     });
