0d8a09cff98743ebdb6ef57e065b2194cf785a13
fuutot <fuutt659@gmail.com>
2025-09-18 17:32:28 +0900

    fix: 要素ごとにボタンを作成

--- src/content.js
+++ src/content.js
@@ -1,7 +1,3 @@
-// ボタン要素を作成
-const button = document.createElement('button');
-button.textContent = '新規Scrap作成';
-
 // ボタンを追加するターゲット要素を取得
 const targetSelector = '.ThreadEditor_buttons__Y_Bk5';
 const targets = document.querySelectorAll(targetSelector);
@@ -9,13 +5,14 @@ const targets = document.querySelectorAll(targetSelector);
 // ターゲット要素が存在する場合、ボタンを追加
 if (targets.length > 0) {
   targets.forEach(target => {
+    // 要素ごとにボタンを作成して追加
+    const button = document.createElement('button');
+    button.textContent = '新規Scrap作成';
+    button.addEventListener('click', () => {
+      alert('ボタンがクリックされました');
+    });
     target.appendChild(button);
   });
 } else {
   console.log(`ターゲット要素が見つかりません: ${targetSelector}`);
 }
-
-// ボタンのクリックイベント
-button.addEventListener('click', () => {
-  alert('ボタンがクリックされました');
-});
