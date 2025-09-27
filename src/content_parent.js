c7d1b51264391597a54063de47fc9b0d6fd5ae2e
fuutot <fuutt659@gmail.com>
2025-09-27 21:22:05 +0900

    add: 子Scrapが投稿された時に親Scrapにアラートを表示

--- src/content_parent.js
+++ src/content_parent.js
@@ -1,1 +1,9 @@
 console.log('Content Parent Script Loaded');
+
+// 子Scrap記事の投稿時にアラートを表示
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === 'childScrapPosted') {
+        const childUrl = message.url || '不明なURL';
+        alert(`子記事が投稿されました！\n記事URL: ${childUrl}`);
+    }
+});
